dbf4034c1218f59db98f2dd832df1c68dc1963c7
Code Sandbox <hello@codesandbox.io>
2022-10-24 14:03:10 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a186b8aa030e418ced55ec52cd28158e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33227eb0cb9b79bca8ac2da720129f4d",
+    "url": "https://sdepaula97.github.io/csb-hwvw46/index.html"
+  },
+  {
+    "revision": "51d995b77f514c2d679c",
+    "url": "https://sdepaula97.github.io/csb-hwvw46/static/css/main.4e84de7b.chunk.css"
+  },
+  {
+    "revision": "b2eb0103bf124a4ce9cc",
+    "url": "https://sdepaula97.github.io/csb-hwvw46/static/js/2.3c0d2587.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://sdepaula97.github.io/csb-hwvw46/static/js/2.3c0d2587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d995b77f514c2d679c",
+    "url": "https://sdepaula97.github.io/csb-hwvw46/static/js/main.5cb6a31c.chunk.js"
+  },
+  {
+    "revision": "be2e9d3006b53444f29b",
+    "url": "https://sdepaula97.github.io/csb-hwvw46/static/js/runtime-main.865b702c.js"
+  }
+]);
